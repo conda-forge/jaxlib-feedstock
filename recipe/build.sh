39956847db8b4a5ed80d8cb342794496597a00e5
Uwe L. Korn <uwe.korn@quantco.com>
2021-07-19 19:10:58 +0200

    Debug commands on osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,16 @@ else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
 source ${RECIPE_DIR}/gen-bazel-toolchain.sh
+
+CUSTOM_BAZEL_OPTIONS="--crosstool_top=//custom_toolchain:toolchain --logging=6 --verbose_failures"
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  CUSTOM_BAZEL_OPTIONS="${CUSTOM_BAZEL_OPTIONS} --subcommands"
+fi
+
 if [[ "${target_platform}" == linux-* ]]; then
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " --crosstool_top=//custom_toolchain:toolchain --logging=6 --verbose_failures --cpu ${TARGET_CPU}"
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " ${CUSTOM_BAZEL_OPTIONS} --cpu ${TARGET_CPU}"
 else
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " --crosstool_top=//custom_toolchain:toolchain --logging=6 --verbose_failures" --target_cpu ${TARGET_CPU}
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " ${CUSTOM_BAZEL_OPTIONS}" --target_cpu ${TARGET_CPU}
 fi
 ${PYTHON} -m pip install dist/jaxlib-*.whl
