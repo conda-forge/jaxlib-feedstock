90b6ebcafd72014178dd3471a1db5c50a08331b1
Uwe L. Korn <uwe.korn@quantco.com>
2021-07-19 17:51:10 +0200

    Add upstream patch for cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,5 +8,5 @@ else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
 source ${RECIPE_DIR}/gen-bazel-toolchain.sh
-${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " --crosstool_top=//custom_toolchain:toolchain --cpu=${TARGET_CPU} --logging=6 --verbose_failures"
+${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " --crosstool_top=//custom_toolchain:toolchain --logging=6 --verbose_failures" --target_cpu ${TARGET_CPU}
 ${PYTHON} -m pip install dist/jaxlib-*.whl
