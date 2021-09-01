d855de3d4ab75cc17008574da850cdb3efdbdaf4
Uwe L. Korn <uwe.korn@quantco.com>
2021-09-01 17:14:10 +0200

    Remove merged patch, use new CLI

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ else
 fi
 source gen-bazel-toolchain
 
-CUSTOM_BAZEL_OPTIONS="--crosstool_top=//bazel_toolchain:toolchain --logging=6 --verbose_failures"
+CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//bazel_toolchain:toolchain --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug"
 
 if [[ "${target_platform}" == "osx-64" ]]; then
   # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
@@ -17,8 +17,8 @@ if [[ "${target_platform}" == "osx-64" ]]; then
 fi
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " ${CUSTOM_BAZEL_OPTIONS}" --target_cpu ${TARGET_CPU}
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --target_cpu ${TARGET_CPU}
 else
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn --bazel_options " ${CUSTOM_BAZEL_OPTIONS} --cpu ${TARGET_CPU}"
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --bazel_options=--cpu --bazel_options=${TARGET_CPU}
 fi
 ${PYTHON} -m pip install dist/jaxlib-*.whl
