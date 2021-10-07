7ce7d08cf788006054344d4f93d65c65c7d15b46
Daniel Holth <dholth@anaconda.com>
2021-10-07 14:37:18 -0400

    build with bazel 4.2.1 and without bazel-toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ if [[ "${target_platform}" == osx-* ]]; then
 else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
-source gen-bazel-toolchain
+# source gen-bazel-toolchain
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//bazel_toolchain:toolchain --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug"
 
@@ -17,9 +17,9 @@ if [[ "${target_platform}" == "osx-64" ]]; then
 fi
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --target_cpu ${TARGET_CPU}
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} # --target_cpu ${TARGET_CPU}
 else
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --bazel_options=--cpu --bazel_options=${TARGET_CPU}
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} # --bazel_options=--cpu --bazel_options=${TARGET_CPU}
 fi
 
 # Clean up to speedup postprocessing
