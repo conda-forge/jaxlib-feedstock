573ffd34ceae53f22367f98066b8990024eaf5ba
Daniel Holth <dholth@anaconda.com>
2021-10-08 14:47:58 -0400

    include part of bazel-toolchain environment variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,27 @@ if [[ "${target_platform}" == osx-* ]]; then
 else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
-# source gen-bazel-toolchain
+
+# from bazel-toolchain-feedstock
+export TARGET_SYSTEM="${HOST}"
+if [[ "${target_platform}" == "osx-64" ]]; then
+  export TARGET_LIBC="macosx"
+  export TARGET_CPU="darwin_x86_64"
+  export TARGET_SYSTEM="x86_64-apple-macosx"
+elif [[ "${target_platform}" == "osx-arm64" ]]; then
+  export TARGET_LIBC="macosx"
+  export TARGET_CPU="darwin_arm64"
+  export TARGET_SYSTEM="arm64-apple-macosx"
+elif [[ "${target_platform}" == "linux-64" ]]; then
+  export TARGET_LIBC="unknown"
+  export TARGET_CPU="k8"
+elif [[ "${target_platform}" == "linux-aarch64" ]]; then
+  export TARGET_LIBC="unknown"
+  export TARGET_CPU="aarch64"
+elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
+  export TARGET_LIBC="unknown"
+  export TARGET_CPU="ppc"
+fi
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//bazel_toolchain:toolchain --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug"
 
@@ -17,9 +37,9 @@ if [[ "${target_platform}" == "osx-64" ]]; then
 fi
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} # --target_cpu ${TARGET_CPU}
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --target_cpu ${TARGET_CPU}
 else
-  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} # --bazel_options=--cpu --bazel_options=${TARGET_CPU}
+  ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --bazel_options=--cpu --bazel_options=${TARGET_CPU}
 fi
 
 # Clean up to speedup postprocessing
