e5b17507db2c06fac37ddc529a7b9ca8c76ea5a3
Uwe Korn <uwelk@xhochy.com>
2022-02-17 06:29:37 +0000

    Force c++17 everywhere

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,10 @@ set -euxo pipefail
 
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
-  # Force C++17 to workaround symbol visibility issues
-  export CXXFLAGS=${CXXFLAGS/std=c++14/std=c++17}
-  sed -i -e 's/c++14/c++17/g' .bazelrc
 else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
+sed -i -e 's/c++14/c++17/g' .bazelrc
 source gen-bazel-toolchain
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//bazel_toolchain:toolchain --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug --bazel_options=--define=PREFIX=${PREFIX} --bazel_options=--define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include"
