30272cda463f03879d6aca5413896d3fb7fa5f9c
ngam <67342040+ngam@users.noreply.github.com>
2022-05-13 17:20:16 -0400

    fix misc custom_bazel_options

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,6 @@ export INCLUDEDIR=$PREFIX/include
 
 export TF_IGNORE_MAX_BAZEL_VERSION="1"
 
-
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
 else
@@ -22,16 +21,19 @@ export CXXFLAGS="${CXXFLAGS} -DNDEBUG"
 
 if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
    source ${RECIPE_DIR}/gen-bazel-toolchain.sh
+   CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//custom_toolchain:toolchain"
 else
    source gen-bazel-toolchain
+   CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//bazel_toolchain:toolchain"
 fi
 
-CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//custom_toolchain:toolchain --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug --bazel_options=--define=PREFIX=${PREFIX} --bazel_options=--define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include"
+CUSTOM_BAZEL_OPTIONS="${CUSTOM_BAZEL_OPTIONS} --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug --bazel_options=--define=PREFIX=${PREFIX} --bazel_options=--define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include"
+
 # For debugging
 # CUSTOM_BAZEL_OPTIONS="${CUSTOM_BAZEL_OPTIONS} --bazel_options=--subcommands"
 
 if [[ "${target_platform}" == "osx-64" ]]; then
-#   Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
+  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
   TARGET_CPU=darwin
 fi
 
@@ -66,14 +68,13 @@ fi
 #
 # Thus: don't add com_google_protobuf here.
 # FIXME: Current global abseil pin is too old for jaxlib, readd com_google_absl once we are on a newer version.
+export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,com_github_grpc_grpc,flatbuffers,zlib"
+
 if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
     export BAZEL_LINKLIBS="-lstdc++"
-    export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,com_github_grpc_grpc,flatbuffers,zlib"
     export GCC_HOST_COMPILER_PATH="${GCC}"
     export GCC_HOST_COMPILER_PREFIX="$(dirname ${GCC})"
     export LDFLAGS="${LDFLAGS//-Wl,-z,now/-Wl,-z,lazy}"
-else
-    export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,com_github_grpc_grpc,flatbuffers,zlib"
 fi
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
