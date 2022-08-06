0f249a569eb26f4a30c1fabb9676782b62ca3b74
ngam <67342040+ngam@users.noreply.github.com>
2022-08-06 12:51:54 -0400

    disable libprotobuf and grpc-cpp

--- recipe/build.sh
+++ recipe/build.sh
@@ -62,7 +62,7 @@ fi
 #
 # Thus: don't add com_google_protobuf here.
 # FIXME: Current global abseil pin is too old for jaxlib, readd com_google_absl once we are on a newer version.
-export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,com_github_grpc_grpc,flatbuffers,zlib"
+export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,flatbuffers,zlib"
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --target_cpu ${TARGET_CPU}
