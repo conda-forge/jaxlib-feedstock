d2d85176c7278958525a7581cb4c6ce37ae3bf1e
ngam <67342040+ngam@users.noreply.github.com>
2022-08-09 14:06:54 -0400

    try copying settings from tf again

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,17 +2,46 @@
 
 set -euxo pipefail
 
+export PATH="$PWD:$PATH"
+export CC=$(basename $CC)
+export CXX=$(basename $CXX)
+export LIBDIR=$PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
+export PYTHON_BIN_PATH=${PYTHON}
+export PYTHON_LIB_PATH=${SP_DIR}
+export USE_DEFAULT_PYTHON_LIB_PATH=1
+
+export TF_IGNORE_MAX_BAZEL_VERSION="1"
+
+mkdir -p ./bazel_output_base
+export BAZEL_OPTS=""
+# Set this to something as otherwise, it would include CFLAGS which itself contains a host path and this then breaks bazel's include path validation.
+export CC_OPT_FLAGS="-O2"
+
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
 else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
+
 sed -i -e 's/c++14/c++17/g' .bazelrc
+# Get rid of unwanted defaults
+sed -i -e "/PROTOBUF_INCLUDE_PATH/c\ " .bazelrc
+sed -i -e "/PREFIX/c\ " .bazelrc
+
 export CFLAGS="${CFLAGS} -DNDEBUG"
 export CXXFLAGS="${CXXFLAGS} -DNDEBUG"
 source gen-bazel-toolchain
 
-CUSTOM_BAZEL_OPTIONS="--bazel_options=--crosstool_top=//bazel_toolchain:toolchain --bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug --bazel_options=--define=PREFIX=${PREFIX} --bazel_options=--local_cpu_resources=${CPU_COUNT}"
+CUSTOM_BAZEL_OPTIONS="
+  --bazel_options=--crosstool_top=//bazel_toolchain:toolchain 
+  --bazel_options=--logging=6 
+  --bazel_options=--verbose_failures 
+  --bazel_options=--config=opt 
+  --bazel_options=--toolchain_resolution_debug 
+  --bazel_options=--define=PREFIX=${PREFIX} 
+  --bazel_options=--local_cpu_resources=${CPU_COUNT} "
 # For debugging
 # CUSTOM_BAZEL_OPTIONS="${CUSTOM_BAZEL_OPTIONS} --bazel_options=--subcommands"
 
@@ -41,6 +70,9 @@ if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
     export TF_NEED_CUDA=1
     export TF_NCCL_VERSION=$(pkg-config nccl --modversion | grep -Po '\d+\.\d+')
 
+    export LDFLAGS="${LDFLAGS//-Wl,-z,now/-Wl,-z,lazy}"
+    export CC_OPT_FLAGS="-march=nocona -mtune=haswell"
+  
     CUDA_ARGS="--enable_cuda \
                --enable_nccl \
                --cuda_path=$CUDA_HOME \
@@ -55,13 +87,17 @@ fi
 #
 # Thus: don't add com_google_protobuf here.
 # FIXME: Current global abseil pin is too old for jaxlib, readd com_google_absl once we are on a newer version.
-# export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,com_github_grpc_grpc,flatbuffers,zlib"
+export TF_SYSTEM_LIBS="boringssl,com_github_googlecloudplatform_google_cloud_cpp,com_github_grpc_grpc,flatbuffers,zlib"
+
+bazel clean --expunge
+bazel shutdown
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --target_cpu ${TARGET_CPU}
 else
   ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --bazel_options=--cpu --bazel_options=${TARGET_CPU} ${CUDA_ARGS:-}
 fi
+
 
 # Clean up to speedup postprocessing
 pushd build
@@ -72,3 +108,5 @@ pushd $SP_DIR
 # pip doesn't want to install cleanly in all cases, so we use the fact that we can unzip it.
 unzip $SRC_DIR/dist/jaxlib-*.whl
 popd
+
+bazel clean
