b4f2d76b7afcf9f40af6065133a804f6ec52def4
Eric Lundby <Eric.Lundby@gmail.com>
2022-11-21 15:38:19 -0700

    Call bazel shutdown, add some output.

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,11 +11,18 @@ fi
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug"
 
+echo "Building...."
 ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS}
+echo "Building done."
 
 # Clean up to speedup postprocessing
+echo "Issuing bazel clean..."
 pushd build
 bazel clean
 popd
 
+echo "Issuing bazel shutdown..."
+bazel shutdown
+
+echo "Installing jaxlib wheel..."
 ${PYTHON} -m pip install dist/jaxlib-*.whl
