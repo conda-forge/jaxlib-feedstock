d6eacf35e425157afb9ec5b2993a27344e806197
Eric Lundby <Eric.Lundby@gmail.com>
2022-11-21 09:47:03 -0700

    Don't change TARGET_CPU for osx-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,10 +31,11 @@ fi
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug"
 
-if [[ "${target_platform}" == "osx-64" ]]; then
-  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
-  TARGET_CPU=darwin
-fi
+# if [[ "${target_platform}" == "osx-64" ]]; then
+#   # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
+#   TARGET_CPU=darwin
+# fi
+
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   ${PYTHON} build/build.py --target_cpu_features default --enable_mkl_dnn ${CUSTOM_BAZEL_OPTIONS} --target_cpu ${TARGET_CPU}
