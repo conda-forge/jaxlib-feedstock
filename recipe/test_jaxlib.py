8c786957d3e8a795cac491b866375714282a2b7f
ngam <67342040+ngam@users.noreply.github.com>
2023-01-30 17:35:24 -0500

    try new test

--- recipe/test_jaxlib.py
+++ recipe/test_jaxlib.py
@@ -1,11 +1,24 @@
-import numpy as np
-from jaxlib import xla_client as xc
-xops = xc.ops
+# import numpy as np
+# from jaxlib import xla_client as xc
+# xops = xc.ops
 
-c = xc.XlaBuilder("simple_scalar")
-param_shape = xc.Shape.array_shape(np.dtype(np.float32), ())
-x = xops.Parameter(c, 0, param_shape)
-y = xops.Sin(x)
-computation = c.Build()
+# c = xc.XlaBuilder("simple_scalar")
+# param_shape = xc.Shape.array_shape(np.dtype(np.float32), ())
+# x = xops.Parameter(c, 0, param_shape)
+# y = xops.Sin(x)
+# computation = c.Build()
+# cpu_backend = xc.make_cpu_client()
+# compiled_computation = cpu_backend.compile(computation)
+
+from jaxlib import xla_client as xc
+
 cpu_backend = xc.make_cpu_client()
-compiled_computation = cpu_backend.compile(computation)
+compiled_computation = cpu_backend.compile(
+"""
+module @simple_scalar {
+  func.func @main(%arg0: tensor<f32>) -> tensor<f32> {
+    %0 = stablehlo.sine %arg0 : tensor<f32>
+    return %0 : tensor<f32>
+  }
+}
+""")
