aba36a1fd14809b48c2353d56a03391440e40f59
Steven <scrass@anaconda.com>
2024-01-11 16:14:16 -0700

    add bazel-toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,17 @@ else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
 
+source gen-bazel-toolchain
+
+cat >> .bazelrc <<EOF
+build --crosstool_top=//bazel_toolchain:toolchain
+build --logging=6
+build --verbose_failures
+build --toolchain_resolution_debug
+build --define=PREFIX=${PREFIX}
+build --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
+build --local_cpu_resources=${CPU_COUNT}"
+EOF
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--logging=6 --bazel_options=--verbose_failures --bazel_options=--toolchain_resolution_debug"
 
