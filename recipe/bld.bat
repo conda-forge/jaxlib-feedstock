b407efbec1ff740e739bbd1af1ed90e0c6b0aeb1
Uwe L. Korn <uwe.korn@quantco.com>
2024-06-25 16:22:24 +0200

    Add build script for Windows

---
+++ recipe/bld.bat
@@ -0,0 +1,19 @@
+@echo on
+
+set "BAZEL_SH=%BUILD_PREFIX%\Library\usr\bin\bash.exe"
+
+%PYTHON% build/build.py --target_cpu_features default --enable_mkl_dnn
+if %ERRORLEVEL% neq 0 exit 1
+
+@rem Clean up to speedup postprocessing
+pushd build
+bazel clean
+popd
+
+pushd %SP_DIR%
+exit 1
+@rem pip doesn't want to install cleanly in all cases, so we use the fact that we can unzip it.
+@rem Fix this for Windows
+@rem unzip $SRC_DIR/dist/jaxlib-*.whl
+popd
+
