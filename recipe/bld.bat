e724644063e4777dbf52d27a67f62618802b9760
Uwe L. Korn <uwe.korn@quantco.com>
2024-07-11 10:41:07 +0200

    Add windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,25 @@
+@echo on
+
+set "BAZEL_SH=%BUILD_PREFIX%\Library\usr\bin\bash.exe"
+
+echo "build --local_cpu_resources=1" >> .bazelrc
+echo "--extra_toolchains=@local_config_cc//:cc-toolchain-x64_windows-clang-cl" >> .bazelrc
+echo "--extra_execution_platforms=//jax/tools/toolchains:x64_windows-clang-cl" >> .bazelrc
+echo "--compiler=clang-cl" >> .bazelrc
+
+type .bazelrc
+
+%PYTHON% build/build.py --target_cpu_features default --enable_mkl_dnn
+if %ERRORLEVEL% neq 0 exit 1
+
+@rem Clean up to speedup postprocessing
+pushd build
+bazel clean
+popd
+
+pushd %SP_DIR%
+exit 1
+@rem pip doesn't want to install cleanly in all cases, so we use the fact that we can unzip it.
+@rem Fix this for Windows
+@rem unzip $SRC_DIR/dist/jaxlib-*.whl
+popd
