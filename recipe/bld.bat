261b249a3b2895b4253b0793486a335b8f9d05f8
Eric Lundby <Eric.Lundby@gmail.com>
2024-10-22 10:23:30 -0600

    Try to fix overlinking.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,3 +3,5 @@ bazel clean --expunge
 bazel shutdown
 
 %PYTHON% -m pip install --find-links=dist jaxlib --no-build-isolation --no-deps
+
+copy %PREFIX%\Lib\site-packages\jaxlib\mlir\_mlir_libs\*.dll %LIBRARY_BIN%\
