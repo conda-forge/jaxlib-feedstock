4d5d5d7a01c456210222db041c3fccdf3aacf761
Eric Lundby <Eric.Lundby@gmail.com>
2024-10-23 15:51:57 -0600

    Trying to fix windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,9 @@
-%PYTHON% build/build.py --use_clang=false
+set BAZEL_VS=C:/Program Files/Microsoft Visual Studio/2022/BuildTools 
+set BAZEL_VC=C:/Program Files/Microsoft Visual Studio/2022/BuildTools/VC
+set CLANG_COMPILER_PATH=%BUILD_PREFIX:\=/%/Library/bin/clang.exe
+set BAZEL_LLVM=%BUILD_PREFIX:\=/%/Library/
+
+%PYTHON% build/build.py --bazel_options=--config=win_clang --verbose --use_clang=true --clang_path=%BUILD_PREFIX:\=/%/Library/bin/clang.exe
 bazel clean --expunge
 bazel shutdown
 
