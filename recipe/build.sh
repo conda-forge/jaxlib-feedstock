fda1537262202ee0db84b490681065a735aa4a4d
Eric Lundby <Eric.Lundby@gmail.com>
2024-11-01 10:24:31 -0600

    Remove config that should be populated already

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,8 +32,6 @@ build --toolchain_resolution_debug
 build --define=PREFIX=${PREFIX}
 build --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
 build --local_cpu_resources=${CPU_COUNT}"
-build --define=with_cuda=false
-build --cxxopt=-I${PREFIX}/include
 EOF
 
 CUSTOM_BAZEL_OPTIONS="--bazel_options=--logging=6 --bazel_options=--verbose_failures"
