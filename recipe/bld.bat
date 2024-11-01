bcffa015aefbc76dd6b7b33dbc2ce1796147a9a1
Eric Lundby <Eric.Lundby@gmail.com>
2024-11-01 15:28:57 -0600

    Add a note about TF_SYSTEM_LIBS on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,8 @@ set BAZEL_VC="C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC"
 set CLANG_COMPILER_PATH=%BUILD_PREFIX:\=/%/Library/bin/clang.exe
 set BAZEL_LLVM=%BUILD_PREFIX:\=/%/Library/
 
+@REM Note: TF_SYSTEM_LIBS don't work on windows per https://github.com/openxla/xla/blob/edf18ce242f234fbd20d1fbf4e9c96dfa5be2847/.bazelrc#L383
+
 %PYTHON% build/build.py --bazel_options=--config=win_clang --verbose --use_clang=true --clang_path=%CLANG_COMPILER_PATH%
 bazel clean --expunge
 bazel shutdown
