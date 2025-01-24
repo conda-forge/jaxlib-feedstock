2dd3c2f1d0a605e5c1f76523d755bddf3ffc3fb4
Uwe L. Korn <uwe.korn@quantco.com>
2025-01-24 11:25:29 +0100

    Add real sed

--- recipe/add_py_toolchain.sh
+++ recipe/add_py_toolchain.sh
@@ -2,6 +2,8 @@
 #
 # Create a Python toolchain in the current working directory.
 
+set -exuo pipefail
+
 mkdir -p py_toolchain
 cp $RECIPE_DIR/py_toolchain.bzl py_toolchain/BUILD
 sed -i "s;@@SRC_DIR@@;$SRC_DIR;" py_toolchain/BUILD
