7ae97bd7a40ae8cd87e77fb8b6f9569e2f57ba37
Uwe L. Korn <uwe.korn@quantco.com>
2025-10-07 20:13:01 +0200

    bazel-toolchain is unix-only

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,7 +4,9 @@ set "PREFIX_CYG=%PREFIX:\=/%"
 set "PREFIX_CYG=/%PREFIX_CYG::=%"
 
 echo "build --repo_env=GRPC_BAZEL_DIR=%PREFIX_CYG%/share/bazel/grpc/bazel" >> .bazelrc
+echo "" >> .bazelrc
 if %ERRORLEVEL% neq 0 exit 1
+type .bazelrc
 
 %PYTHON% build/build.py build --target_cpu_features default --wheels=jaxlib
 if %ERRORLEVEL% neq 0 exit 1
