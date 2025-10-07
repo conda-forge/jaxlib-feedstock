9fadf78e10e0a47c66eca0279df096271d579a3d
Uwe L. Korn <uwe.korn@quantco.com>
2025-10-07 17:44:52 +0200

    Set GRPC_BAZEL_DIR

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,4 +1,8 @@
 @echo on
+
+echo "build --repo_env=GRPC_BAZEL_DIR=%PREFIX%/share/bazel/grpc/bazel" >> .bazelrc
+if %ERRORLEVEL% neq 0 exit 1
+
 %PYTHON% build/build.py build --target_cpu_features default --wheels=jaxlib
 if %ERRORLEVEL% neq 0 exit 1
 
